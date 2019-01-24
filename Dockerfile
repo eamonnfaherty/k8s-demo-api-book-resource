9702ad71b0a3d3e011af464b5cfddb118b818816
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-01-24 23:18:55 +0000

    initial add - no data store yet

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.7.2-alpine3.8
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+EXPOSE 8000
+
+COPY requirements.txt /usr/src/app/
+RUN pip install -r requirements.txt
+
+COPY api.py /usr/src/app/
+CMD [ "hug", "-f", "api.py" ]
---
+++ api.py
@@ -0,0 +1,15 @@
+"""API to expose book data"""
+import hug
+
+book_db = {
+  1111111111111: {
+      "isbn": 1111111111111,
+      "name": "amazing book about numbers" 
+  }
+}
+
+@hug.get('/book_by_isbn/{isbn}')
+def book_by_isbn(isbn:int):
+    """get a book via the isbn number"""
+    return book_db.get(isbn)
+
