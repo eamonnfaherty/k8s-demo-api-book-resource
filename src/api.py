dfc9472ba5cae7c6c0284703ae427778aa229b68
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-01-28 21:35:25 +0000

    adding chart and fixing api

--- src/api.py
+++ src/api.py
@@ -1,12 +1,6 @@
 """API to expose book data"""
 import hug
-
-book_db = {
-    1111111111111: {
-        "isbn": 1111111111111,
-        "name": "amazing book about numbers"
-    }
-}
+import handler
 
 
 @hug.get('/')
@@ -17,4 +11,4 @@ def root():
 @hug.get('/by_isbn/{isbn}')
 def book_by_isbn(isbn: int):
     """get a book via the isbn number"""
-    return book_db.get(isbn)
+    return handler.book_by_isbn(isbn)
